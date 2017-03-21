f1ec54776f8bfea1728c7d18e381030cb422fd10
Cyrus Liu <Cyrus@Cyrus-MBP.fios-router.home>
2017-03-21 12:41:03 -0400

    obstacle avoidance, has backpass issue

--- DriftCar_gen_files/iLQG_problem.h
+++ DriftCar_gen_files/iLQG_problem.h
@@ -1,4 +1,4 @@
-/* File generated form template iLQG_problem.tem on 2017-03-21 12:19:40-04:00. Do not edit! */
+/* File generated form template iLQG_problem.tem on 2017-03-21 12:32:41-04:00. Do not edit! */
 
 #ifndef ILQG_PROBLEM_H
 #define ILQG_PROBLEM_H
--- testDriftCar.m
+++ testDriftCar.m
@@ -22,7 +22,7 @@ p.cf= [ 10 10 1 .1 .1 .1];
 p.pf= [ .01 .01 .1 .1 .1 .1];
 
 p.cx  = 1e-1*[5  5 4];          % running cost coefficients 
-p.cdx = 1e-2*[0.5 0.5 0.2];
+p.cdx = 1e-1*[5 0.5 0.2];
 p.px  = [.01 .01 .1];   % smoothness scales for running cost
 
 p.cdrift = -0.001;
@@ -32,18 +32,24 @@ p.limSteer= [-0.68  0.76];
 
 p.xDes = [3 0 0 0 0 0];
 
+p.k_pos = 0.5;
+p.k_vel = 0.1;
+p.d_thres = 0.3;
+p.Obs = [1 0];
+
 %% initial conditions
 T= 50;              % horizon
 t= (1:501)*p.h;
-x0= [0;0;0;0;0;0;0;0;0;0];   % initial state
+x0= [0;0;0;3;0;0;0;0;0;0];   % initial state
 u0(1,:) = 0.25*randn(1,T) +2; % commanded speed
 u0(2,:) = 0.1*randn(1,T); % steering
 
-Op.max_iter= 50;
+Op.max_iter= 100;
 
 tic
 [success, x, u, cost]= iLQGDriftCar(x0, u0, p, Op);
 toc
 
 figure
+init_plot(x0,p.xDes,p.Obs);
 car_plot(x,u);
