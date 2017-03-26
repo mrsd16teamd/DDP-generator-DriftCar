ee01d843591e3d7af92d75763dc60b607ced26a7
Cyrus Liu <Cyrus@Cyrus-MBP.wv.cc.cmu.edu>
2017-03-26 13:05:11 -0400

    faster code for function definition

--- DriftCar_gen_files/iLQG_problem.h
+++ DriftCar_gen_files/iLQG_problem.h
@@ -1,4 +1,4 @@
-/* File generated form template iLQG_problem.tem on 2017-03-21 12:32:41-04:00. Do not edit! */
+/* File generated form template iLQG_problem.tem on 2017-03-25 10:51:16-04:00. Do not edit! */
 
 #ifndef ILQG_PROBLEM_H
 #define ILQG_PROBLEM_H
@@ -46,88 +46,7 @@ typedef struct {
     double fuu[N_X*sizeofQuu];
     double fxu[N_X*sizeofQxu];
 #endif
-    double s6;
-    double s5;
-    double s4;
-    double s3;
-    double s2;
-    double s1;
 
-    double diff_s6_Ux;
-    double diff_2s6_Ux_Ux;
-    double diff_2s6_Ux_Uy;
-    double diff_2s6_Ux_r;
-    double diff_2s6_Ux_steer;
-    double diff_2s6_Ux_thr;
-    double diff_s6_Uy;
-    double diff_2s6_Uy_Uy;
-    double diff_2s6_Uy_r;
-    double diff_2s6_Uy_steer;
-    double diff_2s6_Uy_thr;
-    double diff_s6_r;
-    double diff_2s6_r_r;
-    double diff_2s6_r_steer;
-    double diff_2s6_r_thr;
-    double diff_s6_steer;
-    double diff_2s6_steer_steer;
-    double diff_s6_thr;
-    double diff_2s6_thr_thr;
-    double diff_s5_Ux;
-    double diff_2s5_Ux_Ux;
-    double diff_2s5_Ux_Uy;
-    double diff_2s5_Ux_r;
-    double diff_2s5_Ux_steer;
-    double diff_2s5_Ux_thr;
-    double diff_s5_Uy;
-    double diff_2s5_Uy_Uy;
-    double diff_2s5_Uy_r;
-    double diff_2s5_Uy_steer;
-    double diff_2s5_Uy_thr;
-    double diff_s5_r;
-    double diff_2s5_r_r;
-    double diff_2s5_r_steer;
-    double diff_2s5_r_thr;
-    double diff_s5_steer;
-    double diff_2s5_steer_steer;
-    double diff_s5_thr;
-    double diff_2s5_thr_thr;
-    double diff_s4_Ux;
-    double diff_2s4_Ux_Ux;
-    double diff_2s4_Ux_Uy;
-    double diff_2s4_Ux_r;
-    double diff_2s4_Ux_steer;
-    double diff_2s4_Ux_thr;
-    double diff_s4_Uy;
-    double diff_2s4_Uy_Uy;
-    double diff_2s4_Uy_r;
-    double diff_2s4_Uy_steer;
-    double diff_2s4_Uy_thr;
-    double diff_s4_r;
-    double diff_2s4_r_r;
-    double diff_2s4_r_steer;
-    double diff_2s4_r_thr;
-    double diff_s4_steer;
-    double diff_2s4_steer_steer;
-    double diff_s4_thr;
-    double diff_2s4_thr_thr;
-    double diff_s2_Ux;
-    double diff_2s2_Ux_Ux;
-    double diff_2s2_Ux_Uy;
-    double diff_2s2_Ux_phi;
-    double diff_s2_Uy;
-    double diff_2s2_Uy_Uy;
-    double diff_2s2_Uy_phi;
-    double diff_s2_phi;
-    double diff_2s2_phi_phi;
-    double diff_s1_Ux;
-    double diff_2s1_Ux_Ux;
-    double diff_2s1_Ux_Uy;
-    double diff_2s1_Ux_phi;
-    double diff_s1_Uy;
-    double diff_2s1_Uy_Uy;
-    double diff_2s1_Uy_phi;
-    double diff_s1_phi;
-    double diff_2s1_phi_phi;
 } trajEl_t;
 
 typedef struct {
