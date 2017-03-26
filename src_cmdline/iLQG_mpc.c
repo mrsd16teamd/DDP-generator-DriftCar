77713203877e5fa1ca92ba4f73345cbc3a0bd649
Cyrus Liu <Cyrus@Cyrus-MBP.wv.cc.cmu.edu>
2017-03-26 13:03:53 -0400

    fix pointer handling

--- src_cmdline/iLQG_mpc.c
+++ src_cmdline/iLQG_mpc.c
@@ -12,6 +12,13 @@
 #include "printMat.h"
 #include "matMult.h"
 
+double* assignParam(double* values, int numVal) {
+    double* temp = malloc(numVal*sizeof(double));
+    memcpy(temp, values, numVal*sizeof(double));
+
+    return temp;
+}
+
 void init_params(tOptSet *o)
 {
     // Set model parameters
@@ -25,62 +32,65 @@ void init_params(tOptSet *o)
     double G_f = m*g*b/L;
     double G_r = m*g*a/L;
 
-    double c_x = 116;
-    double c_a = 197;
+    double c_x = 116.0;
+    double c_a = 197.0;
     double Iz = 0.025;
     double mu = 1.31;
     double mu_s = 0.5;
 
-    double cu[] = {1e-2, 1e-2};
-    double cdu[] = {1e-1, 15e-1};
+    double cu[2] = {1e-2, 1e-2};
+    double cdu[2] = {1e-1, 15e-1};
 
-    double cf[] = {10, 10, 1, 0.1, 0.1, 0.1};
-    double pf[] = {0.01, 0.01, 0.1, 0.1, 0.1, 0.1};
+    double cf[6] = {10, 10, 1, 0.1, 0.1, 0.1};
+    double pf[6] = {0.01, 0.01, 0.1, 0.1, 0.1, 0.1};
 
-    double cx[] = {5e-2, 5e-2, 4e-2};
-    double cdx[] = {5e-1, 5e-2, 2e-2};
-    double px[] = {0.01, 0.01, 0.1};
+    double cx[3] = {5e-2, 5e-2, 4e-2};
+    double cdx[3] = {5e-1, 5e-2, 2e-2};
+    double px[3] = {0.01, 0.01, 0.1};
 
     double cdrift = -0.001;
 
-    double k_pos = 0.1;
-    double k_vel = 0;
+    double k_pos = 0.5;
+    double k_vel = 0.1;
     double d_thres = 0.3;
 
 
-    double limThr[] = {0, 4};
-    double limSteer[] = {-0.68, 0.76};
+    double limThr[2] = {0.0, 4.0};
+    double limSteer[2] = {-0.68, 0.76};
 
-    double xDes[] = {3, 0, 0, 3, 0, 0};
-    double Obs[] = {1, 0};
+    double xDes[6] = {3.0, 0.0, 0.0, 3.0, 0.0, 0.0};
+
+    double Obs[2] = {1.0, 0.0};
 
     o->p= malloc(n_params*sizeof(double *));
-    o->p[0] = &G_f;
-    o->p[1] = &G_r;
-    o->p[2] = &Iz;
-    o->p[3] = Obs;
-    o->p[4] = &a;
-    o->p[5] = &b;
-    o->p[6] = &c_a;
-    o->p[7] = &c_x;
-    o->p[8] = &cdrift;
-    o->p[9] = cdu;
-    o->p[10] = cdx;
-    o->p[11] = cf;
-    o->p[12] = cu;
-    o->p[13] = cx;
-    o->p[14] = &d_thres;
-    o->p[15] = &h;
-    o->p[16] = &k_pos;
-    o->p[17] = &k_vel;
-    o->p[18] = limSteer;
-    o->p[19] = limThr;
-    o->p[20] = &m;
-    o->p[21] = &mu;
-    o->p[22] = &mu_s;
-    o->p[23] = pf;
-    o->p[24] = px;
-    o->p[25] = xDes;
+    o->p[0] = assignParam(&G_f,1);
+    o->p[1] = assignParam(&G_r,1);;
+    o->p[2] = assignParam(&Iz,1);;
+    o->p[3] = assignParam(Obs,2);
+    o->p[4] = assignParam(&a,1);
+    o->p[5] = assignParam(&b,1);
+    o->p[6] = assignParam(&c_a,1);
+    o->p[7] = assignParam(&c_x,1);
+    o->p[8] = assignParam(&cdrift,1);
+    o->p[9] = assignParam(cdu,2);
+    o->p[10] = assignParam(cdx,3);
+    o->p[11] = assignParam(cf,6);
+    o->p[12] = assignParam(cu,2);
+    o->p[13] = assignParam(cx,3);
+    o->p[14] = assignParam(&d_thres,1);
+    o->p[15] = assignParam(&h,1);
+    o->p[16] = assignParam(&k_pos,1);
+    o->p[17] = assignParam(&k_vel,1);
+    o->p[18] = assignParam(limSteer,2);
+    o->p[19] = assignParam(limThr,2);
+    o->p[20] = assignParam(&m,1);
+    o->p[21] = assignParam(&mu,1);
+    o->p[22] = assignParam(&mu_s,1);
+    o->p[23] = assignParam(pf,6);
+    o->p[24] = assignParam(px,3);
+    o->p[25] = assignParam(xDes,6);
+
+    // printf("%f", o->p[3][0]);
 }
 
 int main()
@@ -100,7 +110,7 @@ int main()
     char *err_msg, *fname;
     clock_t begin, end;
 
-    double x0[] = {0,0,0, 3,0,0, 3,0,0,0};
+    double x0[] = {0,0,0, 3,0,0,3,0,0,0};
     
 
     n= sizeof(x0)/sizeof(x0[0]);  // length of state vector
@@ -110,7 +120,7 @@ int main()
     srand(time(NULL));
     for(int i=0; i<N-1; i++) {
         u0[i*m] = ((double)rand()/(double)(RAND_MAX)) * 0.5 + 3;
-        u0[i*m+1] = ((double)rand()/(double)(RAND_MAX)) * 0.2 + 0.3;
+        u0[i*m+1] = ((double)rand()/(double)(RAND_MAX)) * 0.2 + 0.1;
     }
 
     // inputs
@@ -178,6 +188,10 @@ int main()
         }
     }
 
+    for(int i=0; i<n_params; i++) {
+        free(o.p[i]);
+    }
+
     free(o.p);
     for(i= 0; i<NUMBER_OF_THREADS+1; i++)
         free(o.trajectories[i].t);
@@ -187,4 +201,13 @@ int main()
         fprintf(U, "%f, %f\n", u_new[i*m], u_new[i*m+1]);
     }
     fclose(U);
+
+    FILE *X = fopen("X.csv", "w");
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<n; j++) {
+            fprintf(X, "%f,", x_new[i*n+j]);
+        }
+        fprintf(X,"\n");
+    }
+    fclose(X);
 }
