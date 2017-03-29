5f7606d175a1e646d8d00f110aecdeebef879435
Cyrus Liu <Cyrus@Cyrus-MBP.fios-router.home>
2017-03-29 00:09:05 -0400

    safe handling of control sequence

--- src_cmdline/iLQG_mpc.c
+++ src_cmdline/iLQG_mpc.c
@@ -12,14 +12,22 @@
 #include "printMat.h"
 #include "matMult.h"
 
-double* assignParam(double* values, int numVal) {
+struct trajectory {
+    double* x;  // states
+    double* u;  // controls
+    int m;  // dimension of control
+    int n;  // dimension of states
+    int N;  // state horizon N=T+1
+};
+
+double* assignPtrVal(double* values, int numVal) {
     double* temp = malloc(numVal*sizeof(double));
     memcpy(temp, values, numVal*sizeof(double));
 
     return temp;
 }
 
-void init_params(tOptSet *o)
+void init_params(tOptSet *o, double* xDes, double* Obs)
 {
     // Set model parameters
     double g = 9.81;
@@ -39,7 +47,7 @@ void init_params(tOptSet *o)
     double mu_s = 0.5;
 
     double cu[2] = {1e-2, 1e-2};
-    double cdu[2] = {1e-1, 15e-1};
+    double cdu[2] = {10e-1, 15e-1};
 
     double cf[6] = {10, 10, 1, 0.1, 0.1, 0.1};
     double pf[6] = {0.01, 0.01, 0.1, 0.1, 0.1, 0.1};
@@ -50,50 +58,49 @@ void init_params(tOptSet *o)
 
     double cdrift = -0.001;
 
-    double k_pos = 0.5;
-    double k_vel = 0.1;
+    double k_pos = 0.1;
+    double k_vel = 0;
     double d_thres = 0.3;
 
 
-    double limThr[2] = {0.0, 4.0};
+    double limThr[2] = {-1, 4.0};
     double limSteer[2] = {-0.68, 0.76};
 
-    double xDes[6] = {3.0, 0.0, 0.0, 3.0, 0.0, 0.0};
-
-    double Obs[2] = {1.0, 0.0};
+    // double xDes[6] = {3,0,0,3,0,0};
+    // double Obs[2] = {1,0};
 
     o->p= malloc(n_params*sizeof(double *));
-    o->p[0] = assignParam(&G_f,1);
-    o->p[1] = assignParam(&G_r,1);;
-    o->p[2] = assignParam(&Iz,1);;
-    o->p[3] = assignParam(Obs,2);
-    o->p[4] = assignParam(&a,1);
-    o->p[5] = assignParam(&b,1);
-    o->p[6] = assignParam(&c_a,1);
-    o->p[7] = assignParam(&c_x,1);
-    o->p[8] = assignParam(&cdrift,1);
-    o->p[9] = assignParam(cdu,2);
-    o->p[10] = assignParam(cdx,3);
-    o->p[11] = assignParam(cf,6);
-    o->p[12] = assignParam(cu,2);
-    o->p[13] = assignParam(cx,3);
-    o->p[14] = assignParam(&d_thres,1);
-    o->p[15] = assignParam(&h,1);
-    o->p[16] = assignParam(&k_pos,1);
-    o->p[17] = assignParam(&k_vel,1);
-    o->p[18] = assignParam(limSteer,2);
-    o->p[19] = assignParam(limThr,2);
-    o->p[20] = assignParam(&m,1);
-    o->p[21] = assignParam(&mu,1);
-    o->p[22] = assignParam(&mu_s,1);
-    o->p[23] = assignParam(pf,6);
-    o->p[24] = assignParam(px,3);
-    o->p[25] = assignParam(xDes,6);
+    o->p[0] = assignPtrVal(&G_f,1);
+    o->p[1] = assignPtrVal(&G_r,1);;
+    o->p[2] = assignPtrVal(&Iz,1);;
+    o->p[3] = assignPtrVal(Obs,2);
+    o->p[4] = assignPtrVal(&a,1);
+    o->p[5] = assignPtrVal(&b,1);
+    o->p[6] = assignPtrVal(&c_a,1);
+    o->p[7] = assignPtrVal(&c_x,1);
+    o->p[8] = assignPtrVal(&cdrift,1);
+    o->p[9] = assignPtrVal(cdu,2);
+    o->p[10] = assignPtrVal(cdx,3);
+    o->p[11] = assignPtrVal(cf,6);
+    o->p[12] = assignPtrVal(cu,2);
+    o->p[13] = assignPtrVal(cx,3);
+    o->p[14] = assignPtrVal(&d_thres,1);
+    o->p[15] = assignPtrVal(&h,1);
+    o->p[16] = assignPtrVal(&k_pos,1);
+    o->p[17] = assignPtrVal(&k_vel,1);
+    o->p[18] = assignPtrVal(limSteer,2);
+    o->p[19] = assignPtrVal(limThr,2);
+    o->p[20] = assignPtrVal(&m,1);
+    o->p[21] = assignPtrVal(&mu,1);
+    o->p[22] = assignPtrVal(&mu_s,1);
+    o->p[23] = assignPtrVal(pf,6);
+    o->p[24] = assignPtrVal(px,3);
+    o->p[25] = assignPtrVal(xDes,6);
 
     // printf("%f", o->p[3][0]);
 }
 
-int main()
+double* plan_trajectory(double* x0, double* xDes, double* Obs, int T)
 {
     //TODO make o manually
 
@@ -110,12 +117,12 @@ int main()
     char *err_msg, *fname;
     clock_t begin, end;
 
-    double x0[] = {0,0,0, 3,0,0,3,0,0,0};
+    // double x0[] = {0,0,0, 3,0,0,3,0,0,0};
     
 
     n= sizeof(x0)/sizeof(x0[0]);  // length of state vector
     m= 2; // number of inputs
-    N= 51; // T+1 TODO how to make this variable?
+    N= T+1; // T+1 TODO how to make this variable?
     double u0[m*(N-1)]; // TODO row-first
     srand(time(NULL));
     for(int i=0; i<N-1; i++) {
@@ -139,14 +146,16 @@ int main()
     }
 
     // Set model and problem parameters
-    init_params(&o);
+    init_params(&o, xDes, Obs);
 
     // outputs
     double success[1];
     double new_cost[1];
 
-    double x_new[n*N];
-    double u_new[m*(N-1)];
+    double *x_new = malloc(n*N*sizeof(double));
+    double *u_new = malloc(m*(N-1)*sizeof(double));
+    // double x_new[n*N];
+    // double u_new[m*(N-1)];
 
     // aux
     for(i= 0; i<NUMBER_OF_THREADS+1; i++)
@@ -187,6 +196,7 @@ int main()
             new_cost[0]= o.cost;
         }
     }
+    printf("runs to here");
 
     for(int i=0; i<n_params; i++) {
         free(o.p[i]);
@@ -196,18 +206,39 @@ int main()
     for(i= 0; i<NUMBER_OF_THREADS+1; i++)
         free(o.trajectories[i].t);
 
-    FILE *U = fopen("U.csv", "w");
+    printf("Control Sequence: \n");
     for(int i=0; i<N-1; i++) {
-        fprintf(U, "%f, %f\n", u_new[i*m], u_new[i*m+1]);
+        printf("%f, %f\n", u_new[i*m], u_new[i*m+1]);
     }
-    fclose(U);
 
-    FILE *X = fopen("X.csv", "w");
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<n; j++) {
-            fprintf(X, "%f,", x_new[i*n+j]);
-        }
-        fprintf(X,"\n");
+    free(x_new);
+    return u_new;
+}
+
+int main() {
+    int T = 50;
+    double x0[10] = {0,0,0, 3,0,0,3,0,0,0};
+    double xDes[6] = {3.0, 0.0, 0.0, 3.0, 0.0, 0.0};
+    double Obs[2] = {1.0, 0.0};
+
+    int N = T+1;
+    int n = 10;
+    int m = 2;
+
+    double* U;
+    // traj[0]: states, traj[1]: controls
+    U = plan_trajectory(x0,xDes,Obs,50);
+
+    // FILE *U = fopen("U.csv", "w");
+    // for(int i=0; i<N-1; i++) {
+    //     fprintf(U, "%f, %f\n", [i*m], Traj.u[i*m+1]);
+    // }
+    // fclose(U);
+
+    printf("Control Sequence: \n");
+    for(int i=0; i<N-1; i++) {
+        printf("%f, %f\n", U[i*m], U[i*m+1]);
     }
-    fclose(X);
+
+    free(U);
 }
