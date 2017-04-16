ecd29699e40c7fb1596c7529a40c68a6bb482644
Cyrus Liu <Cyrus@Cyrus-MBP.wv.cc.cmu.edu>
2017-04-16 00:49:23 -0400

    update costs and yaml_gen

--- testDriftCar.m
+++ testDriftCar.m
@@ -15,42 +15,42 @@ p.Iz = 0.025; % roatation inertia
 p.mu = 1.31;   
 p.mu_s = 0.55;
 
-p.cu= 1e-1*[1 1];
+p.cu= 1e-2*[1 1];
 p.cdu= 1e-1*[10 15];
 
-p.cf= [ 10 10 1 .1 .1 .1];
+p.cf= [ 10 10 5 10 .1 .1];
 p.pf= [ .01 .01 .1 .1 .1 .1];
 
-p.cx  = 1e-2*[5  5 4];          % running cost coefficients 
-p.cdx = 1e-1*[5 0.5 0.2];
+p.cx  = 1e-2*[5 5 4];          % running cost coefficients 
+p.cdx = 1e-3*[1 5 2];
 p.px  = [.01 .01 .1];   % smoothness scales for running cost
 
 p.cdrift = -0.01;
 
-p.limThr= [-1 4];
+p.limThr= [0 1];
 p.limSteer= [-0.68  0.76];
 
-p.xDes = [3 0 0 3 0 0]; % Moose Test
+p.xDes = [5 0 0 1 0 0]; % Moose Test
 % p.xDes = [1.2 0.5 pi 0 0 0]; % Parallel Park
 
 
-p.k_pos = 0.1;
+p.k_pos = 0.5;
 p.k_vel = 0;
 p.d_thres = 0.3;
 p.Obs = [1 0];
 
 %% initial conditions
-T= 50;              % horizon
-t= (1:501)*p.h;
-x0= [0;0;0;3;0;0;3;0;0;0];   % initial state - Moose Test
+T= 150;              % horizon
+
+x0= [0;0;0;0.1;0;0;0;0;0;0];   % initial state - Moose Test
 % x0= [0;0;0;3.5;0;0;3.5;0;0;0];   % initial state - Parallel Park
 
 
 x00 = x0;
-u0(1,:) = 0.25*randn(1,T) + 3; % commanded speed
+u0(1,:) = 0.25*randn(1,T) + 0.5; % commanded speed
 u0(2,:) = 0.1*randn(1,T) + 0.1; % steering
 
-Op.max_iter= 100;
+Op.max_iter= 300;
 
 figure(1)
 init_plot(x0,p.xDes,p.Obs);
